4015911beb4d0db45e0f154ea11219e7baeff604
MisuBeImp <bsse0516@iit.du.ac.bd>
2016-10-27 00:31:40 +0600

    view parser partially done

--- ViewParser.js
+++ ViewParser.js
@@ -5,133 +5,101 @@
  * Created by Misu Be Imp on 6/9/2016.
  */
 
-var textInEditor;
 
 
-var modelVariables=[];
-var controllerFunctions=[];
-var ngRepeatElements=[];
+var modelVariables = [];
+var controllerFunctions = [];
+var ngRepeatElements = [];
 
-function ModelVariableInView(directive,dataType,modelVariableName) {
+function ModelVariableInView(directive, dataType, modelVariableName) {
     this.directive = directive;
-    this.dataType=dataType;
+    this.dataType = dataType;
     this.modelVariableName = modelVariableName;
 
 }
-function ControllerFunctionInView(directive,dataType,controllerFunctionName)
-{
+function ControllerFunctionInView(directive, dataType, controllerFunctionName) {
     this.directive = directive;
-    this.dataType=dataType;
-    this.controllerFunctionName=controllerFunctionName;
+    this.dataType = dataType;
+    this.controllerFunctionName = controllerFunctionName;
 
 }
-function NG_ReapteElementsInView(arrayName,alice,value)
-{
-    this.arrayName=arrayName;
-    this.alice=alice;
-    this.value=value;
+function NG_ReapteElementsInView(arrayName, alice, value) {
+    this.arrayName = arrayName;
+    this.alice = alice;
+    this.value = value;
 
 }
 
-function getHTMLCode(){
+function getHTMLCode() {
 
     //parse the html into DOM
-    textInEditor = document.getElementById('textEditor').value;
+    var htmlRawCode = document.getElementById('textEditor').value;
     var htmlParser = new DOMParser();
-    var parsedDOM = htmlParser.parseFromString(textInEditor, "text/html");
+    var parsedDOM = htmlParser.parseFromString(htmlRawCode, "text/html");
 
-        /*//find all model variables from view as attribute
-        for (var i = 0; i < angularAttributeDirectivesForModelValue.length; i++) {
-            var directive = angularAttributeDirectivesForModelValue[i];
-            //var element=parsedDOM.querySelector('['+directive.signature+']');
-            var elements = parsedDOM.querySelectorAll('[' + directive.signature + ']');
-
-            if (elements != null && elements.length > 0) {
-                for (var j = 0; j < elements.length; j++) {
-                    var directiveAttributeModelValue = elements[j].getAttribute(directive.signature);
-                    modelVariables.push(new ModelVariableInView(directive.signature,directive.acceptedDatatype,directiveAttributeModelValue.replace(/{{/g, '').replace(/}}/g, '').trim()));
-
-                }
-            }
-        }
-
-        //find all controller functions from the view as attribute
-        for (var i = 0; i < angularAttributeDirectiveForControllerFunctions.length; i++) {
-            var directiveCF = angularAttributeDirectiveForControllerFunctions[i];
-            var elements = parsedDOM.querySelectorAll('[' + directiveCF.signature + ']');
-            if (elements != null && elements.length > 0) {
-                for (var j = 0; j < elements.length; j++) {
-                    var directiveAttributeControllerFunctionValue = elements[j].getAttribute(directiveCF.signature);
-
-                    controllerFunctions.push(new ControllerFunctionInView(directiveCF.signature,directiveCF.acceptedDatatype, directiveAttributeControllerFunctionValue));
-
-                }
-            }
-        }*/
-
-            getModelVariables(parsedDOM);
-            getControllerFunctions(parsedDOM);
-            getNGRepeatElements(parsedDOM);
-
-    /*var attributeValueExpression=/(.*)+="{{(.*)+}}"/g;
-    console.log(typeof attributeValueExpression);
-    console.log(attributeValueExpression);
-*/
-
-        //get all angular Expression {{*}}
-        var allAngularExpression=traversDomNodes(parsedDOM, new Array(), parsedDOM);
-        for (var l = 0; l < allAngularExpression.length; l++) {
-            var element = allAngularExpression[l];
-            var updateElement;
-            if(element.indexOf('.'))
-            {
-                var dotNotation="\\"+"\.";
-                element=element.replace(/\./g,dotNotation);
-            }
-
-            var attributeValueExpression=getAttributeValueRegularExpression(element);
-            var tempMatchList;
-            while ((tempMatchList = attributeValueExpression.exec(textInEditor)) !== null) {
-                var attributeValue = tempMatchList[0];
-                var array=attributeValue.split("=");
-                var attribute=array[0];
-                var value=array[1];
-                for(var j=0; j<angularAttributeDirectivesForModelValue.length;j++)
-                {
-                    var reg=angularAttributeDirectivesForModelValue[j].signature;
-                    var match=eval('/'+reg+'/g');
-                    if(match.exec(attribute))
-                    {
-                        var angularDirective=attribute;
-                    }
-                }
-            }
+    getModelVariables(parsedDOM);
+    getControllerFunctions(parsedDOM);
+    getNGRepeatElements(parsedDOM);
+    getAngularExpressionDirective(htmlRawCode, parsedDOM);
 
 
-
-
-        }
-
-
-
-
-   // }
 }
 
 
+function getAngularExpressionDirective(htmlRawCode, parsedDOM) {
+    //get all angular Expression {{*}}
+    var allAngularExpression = traversDomNodes(parsedDOM, new Array(), parsedDOM);
+    for (var i = 0; i < allAngularExpression.length; i++) {
+        var element = allAngularExpression[i];
+        if (element.indexOf('.')) {
+            var dotNotation = "\\" + "\.";
+            element = element.replace(/\./g, dotNotation);
+        }
 
-function getAttributeValueRegularExpression(value)
-{
-    var firstCaluseExpresion='/'+'ng-'+'\\w*="{{(';
-    var lastCaluseExpressin=')}}"/g'
-    var middleValue=value;
-    var expression= eval(firstCaluseExpresion+middleValue+lastCaluseExpressin);
+        var attributeValueExpression = getAttributeValueRegularExpression(element);
+        var tempMatchList;
+        while ((tempMatchList = attributeValueExpression.exec(htmlRawCode)) !== null) {
+            var attributeValue = tempMatchList[0];
+            var array = attributeValue.split("=");
+            var attribute = array[0];
+            var value = array[1];
+            for (var j = 0; j < angularAttributeDirectivesForModelValue.length; j++) {
+                var regModelDirective = angularAttributeDirectivesForModelValue[j].signature;
+                var matchModelDirective = eval('/' + regModelDirective + '/g');
+                if (matchModelDirective.exec(attribute)) {
+                    var modelDirective = angularAttributeDirectivesForModelValue[j];
+                    var attributeModelValue = value;
+                    modelVariables.push(new ModelVariableInView(modelDirective.signature, modelDirective.acceptedDatatype, attributeModelValue));
+
+                }
+            }
+            for (var k = 0; k < angularAttributeDirectiveForControllerFunctions.length; k++) {
+                var regControllerDirective = angularAttributeDirectiveForControllerFunctions[k].signature;
+                var matchControllerFunctionDirective = eval('/' + regControllerDirective + '/g');
+                if (matchControllerFunctionDirective.exec(attribute)) {
+                    var CFDirective = angularAttributeDirectiveForControllerFunctions[k];
+                    var attributeCFValue = value;
+                    modelVariables.push(new ControllerFunctionInView(CFDirective.signature, CFDirective.acceptedDatatype, attributeCFValue));
+                }
+            }
+        }
+
+
+    }
+
+}
+
+
+function getAttributeValueRegularExpression(value) {
+    var firstExpression = '/' + 'ng-' + '\\w*="{{(';
+    var lastExpression = ')}}"/g'
+    var middleValue = value;
+    var expression = eval(firstExpression + middleValue + lastExpression);
     return expression;
 }
 
-function getNGRepeatElements(parsedDOM)
-{
-    var allNgRepeatElement =parsedDOM.querySelectorAll("[ng-repeat]");
+function getNGRepeatElements(parsedDOM) {
+    var allNgRepeatElement = parsedDOM.querySelectorAll("[ng-repeat]");
     for (var i = 0; i < allNgRepeatElement.length; i++) {
         element = allNgRepeatElement[i].getAttribute('ng-repeat');
         var ngRepeatExpression = element.split("in");
@@ -141,16 +109,13 @@ function getNGRepeatElements(parsedDOM)
         for (var l = 0; l < ngRepeatElement.length; l++) {
             //replace
             var replacedElement = ngRepeatElement[l].replace(alice, modelVariableArray);
-            ngRepeatElements.push(new NG_ReapteElementsInView(modelVariableArray,alice, replacedElement));
+            ngRepeatElements.push(new NG_ReapteElementsInView(modelVariableArray, alice, replacedElement));
         }
-
     }
 
 }
 
-function getModelVariables(parsedDOM)
-{
-
+function getModelVariables(parsedDOM) {
     for (var i = 0; i < angularAttributeDirectivesForModelValue.length; i++) {
         var directive = angularAttributeDirectivesForModelValue[i];
         var elements = parsedDOM.querySelectorAll('[' + directive.signature + ']');
@@ -158,7 +123,7 @@ function getModelVariables(parsedDOM)
         if (elements != null && elements.length > 0) {
             for (var j = 0; j < elements.length; j++) {
                 var directiveAttributeModelValue = elements[j].getAttribute(directive.signature);
-                modelVariables.push(new ModelVariableInView(directive.signature,directive.acceptedDatatype,directiveAttributeModelValue.replace(/{{/g, '').replace(/}}/g, '').trim()));
+                modelVariables.push(new ModelVariableInView(directive.signature, directive.acceptedDatatype, directiveAttributeModelValue.replace(/{{/g, '').replace(/}}/g, '').trim()));
 
             }
         }
@@ -166,8 +131,7 @@ function getModelVariables(parsedDOM)
 
 }
 
-function getControllerFunctions(parsedDOM)
-{
+function getControllerFunctions(parsedDOM) {
     for (var i = 0; i < angularAttributeDirectiveForControllerFunctions.length; i++) {
         var directiveCF = angularAttributeDirectiveForControllerFunctions[i];
         var elements = parsedDOM.querySelectorAll('[' + directiveCF.signature + ']');
@@ -175,7 +139,7 @@ function getControllerFunctions(parsedDOM)
             for (var j = 0; j < elements.length; j++) {
                 var directiveAttributeControllerFunctionValue = elements[j].getAttribute(directiveCF.signature);
 
-                controllerFunctions.push(new ControllerFunctionInView(directiveCF.signature,directiveCF.acceptedDatatype, directiveAttributeControllerFunctionValue));
+                controllerFunctions.push(new ControllerFunctionInView(directiveCF.signature, directiveCF.acceptedDatatype, directiveAttributeControllerFunctionValue));
 
             }
         }
